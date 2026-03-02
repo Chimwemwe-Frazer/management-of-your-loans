e0c8ce0ebd7abeafb3efd31a6b32848ecebf9c08
Chimwemwe-Frazer <chimwemwefrazer10@gmail.com>
2026-03-02 14:32:04 +0200

    Production settings updated

--- config/settings.py
+++ config/settings.py
@@ -25,16 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
-ALLOWED_HOSTS = ['.onrender.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['management-of-your-loans.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://management-of-your-loans.onrender.com"
+    "https://management-of-your-loans.onrender.com",
 ]
 
 
