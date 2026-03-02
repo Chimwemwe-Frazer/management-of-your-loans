7caca3ec8dc05ed2b9aabdb31e1bd9a10f754c9b
Chimwemwe-Frazer <chimwemwefrazer10@gmail.com>
2026-03-02 09:01:03 +0200

    Run collectstatic for production

--- config/settings.py
+++ config/settings.py
@@ -25,13 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com , 127.0.0.1, localhost']
 
 
 # Application definition
